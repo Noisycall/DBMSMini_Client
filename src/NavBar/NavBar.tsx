4ff4a7207c67ee360b98ddb8631e56fc5534e3e8
Noisycall <rajekunal4@gmail.com>
2020-09-11 09:58:17 +0530

    Minor changes to navigation

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,53 +1,53 @@
-import React from "react";
-import {Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import React, { useContext } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../components/AuthContextProvider/AuthContextProvider";
 
-let SLink = ({children, ...rest}: any) => {
+let SLink = ({ children, ...rest }: any) => {
   return (
-      <Link style={{textDecoration: "none", color: "inherit"}} {...rest}>
-        {children}
-      </Link>
+    <Link style={{ textDecoration: "none", color: "inherit" }} {...rest}>
+      {children}
+    </Link>
   );
 };
 const NavBar = () => {
+  let { val } = useContext(AuthContext);
   return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand>
-          <SLink to={"/home"}>Home</SLink>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as="div">
-              <SLink to={"/home"}>Home</SLink>
-            </Nav.Link>
-            <Nav.Link as="div">
-              <SLink to={"/gallery"}>Gallery</SLink>
-            </Nav.Link>
-            <Nav.Link as="div">
-              <SLink to={"/manage"}>Manage</SLink>
-            </Nav.Link>
-            <Nav.Link>
-              <SLink to={"/shop"}>Shop</SLink>
-            </Nav.Link>
-            <Nav.Link as="div">
-              <SLink to={"/register"}>Register</SLink>
-            </Nav.Link>
-            <Nav.Link as="div">
-              <SLink to={"/login"}>Login</SLink>
-            </Nav.Link>
-            <Nav.Link
-                as="div"
-                onClick={() => {
-                  sessionStorage.clear();
-                  window.location.reload();
-                }}
-            >
-              Logout
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>
+        <SLink to={"/home"}>Home</SLink>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link
+            style={{ display: val[0] === "curator" ? "" : "none" }}
+            as="div"
+          >
+            <SLink to={"/manage"}>Manage</SLink>
+          </Nav.Link>
+          <Nav.Link as="div">
+            <SLink to={"/gallery"}>Gallery</SLink>
+          </Nav.Link>
+
+          <Nav.Link>
+            <SLink to={"/shop"}>Shop</SLink>
+          </Nav.Link>
+          <Nav.Link as="div">
+            <SLink to={"/login"}>Login</SLink>
+          </Nav.Link>
+          <Nav.Link
+            as="div"
+            onClick={() => {
+              sessionStorage.clear();
+              window.location.reload();
+            }}
+          >
+            Logout
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 export default NavBar;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,45 +1,57 @@
 import React from "react";
-import {Button, Col, Container, Row} from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  const hist = useHistory();
   return (
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="my-3 text-center">Home</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col lg={6} md={12}>
-            <img src="https://via.placeholder.com/150" alt="placeholder"/>
-            <br/>
-            <br/>
-            <p className="font-weight-bold text-center text-center">
-              Browse our gallery
-            </p>
-            <p className="font-weight-light text-center">
-              Browse high quality artwork from some of the best artists
-            </p>
-            <Button variant="dark" href={"/gallery"}>
-              Browse
-            </Button>
-          </Col>
-          <Col lg={6} md={12}>
-            <img src="https://via.placeholder.com/150" alt="placeholder"/>
-            <br/>
-            <br/>
-            <p className="font-weight-bold text-center">
-              Shop for your favourite art
-            </p>
-            <p className="font-weight-light">
-              Shop for high quality prints in multiple sizes
-            </p>
-            <Button variant="dark" href={"/shop"}>
-              Purchase Now
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+    <Container>
+      <Row>
+        <Col>
+          <h1 className="my-3 text-center">Home</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6} md={12}>
+          <img src="https://via.placeholder.com/150" alt="placeholder" />
+          <br />
+          <br />
+          <p className="font-weight-bold text-center text-center">
+            Browse our gallery
+          </p>
+          <p className="font-weight-light text-center">
+            Browse high quality artwork from some of the best artists
+          </p>
+          <Button
+            variant="dark"
+            onClick={() => {
+              hist.push("/gallery");
+            }}
+          >
+            Browse
+          </Button>
+        </Col>
+        <Col lg={6} md={12}>
+          <img src="https://via.placeholder.com/150" alt="placeholder" />
+          <br />
+          <br />
+          <p className="font-weight-bold text-center">
+            Shop for your favourite art
+          </p>
+          <p className="font-weight-light">
+            Shop for high quality prints in multiple sizes
+          </p>
+          <Button
+            variant="dark"
+            onClick={() => {
+              hist.push("/shop");
+            }}
+          >
+            Purchase Now
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 export default Home;
